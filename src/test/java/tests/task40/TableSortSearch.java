70ad420a27c14a9b400f26b0cc4f3150400c2cea
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-05-04 00:35:44 +0300

    task 40

--- src/test/java/tests/task40/TableSortSearch.java
+++ src/test/java/tests/task40/TableSortSearch.java
@@ -83,46 +83,4 @@ public class TableSortSearch {
         }
         return employees;
     }
-
-    /*private List<Employee> retrieveData(Integer minAge, Double maxSalary){
-        String tableCellPattern = "//table[@id='example']//tbody/tr[%s]/td[%s]";
-        int numOfRow = driver.findElements(By.xpath("//table[@id='example']//tbody/tr")).size();
-        //List<WebElement> elements = driver.findElements(By.xpath("//table[@id='example']//tbody/tr"));
-        List<Employee> employees = new ArrayList<>();
-        for (int iRow = 1; iRow <= numOfRow; iRow++){
-            Employee employee = new Employee();
-            int numOfCell = driver.findElements(By.xpath("//table[@id='example']//tbody/tr[1]/td")).size();
-            for(int iCell = 1; iCell <= numOfCell; iCell++){
-                switch (iCell){
-                    case 1:
-                        String name = driver.findElement(By.xpath(String.format(tableCellPattern, iRow, 1))).getText();
-                        employee.setName(name);
-                        break;
-                    case 2:
-                        String positions = driver.findElement(By.xpath(String.format(tableCellPattern, iRow, 2))).getText();
-                        employee.setPosition(positions);
-                        break;
-                    case 3:
-                        String office = driver.findElement(By.xpath(String.format(tableCellPattern, iRow, 3))).getText();
-                        employee.setOffice(office);
-                        break;
-                    case 4:
-                        Integer age = Integer.valueOf(driver.findElement(By.xpath(String.format(tableCellPattern, iRow, 4))).getText());
-                        employee.setAge(age);
-                        break;
-                    case 6:
-                        String text = driver.findElement(By.xpath(String.format(tableCellPattern, iRow, 6))).getText();
-                        Double salary = Double.valueOf(text.replaceAll("[^0-9]", ""));
-                        employee.setSalary(salary);
-                        break;
-                }
-            }
-            if (employee.getAge() > minAge && employee.getSalary() < maxSalary){
-                employees.add(employee);
-            }
-        }
-        return employees;
-    }*/
-
-
 }
