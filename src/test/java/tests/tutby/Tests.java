0639dab45c600d99fa635a2fbaa84d023f745a7a
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-05-13 22:57:27 +0300

    Task 60 Page Factory pattern

--- src/test/java/tests/tutby/Tests.java
+++ src/test/java/tests/tutby/Tests.java
@@ -1,16 +1,27 @@
 package tests.tutby;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import web_pages.TutByHomePage;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Tests extends BaseStep{
     private TutByHomePage tutByHomePage;
 
     @Test
+    @DisplayName("User is authorized")
+    @Order(1)
     public void verifyAuthorization(){
         tutByHomePage = new TutByHomePage(driver);
         tutByHomePage.loginToSite();
         Assertions.assertEquals(tutByHomePage.extractUserName(), "Selenium Test", "User is authorized");
+    }
+
+    @Test
+    @DisplayName("User is logged out")
+    @Order(2)
+    public void verifyLoginOut(){
+        tutByHomePage = new TutByHomePage(driver);
+        tutByHomePage.logOutFromSite();
+        Assertions.assertTrue(tutByHomePage.authLinkIsDisplayed());
     }
 }
