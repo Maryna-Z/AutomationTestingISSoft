fdc4cb2f482a04c98f0ce880a2a0a5ede870bae4
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-05-14 18:14:59 +0300

    add task 70.

--- src/test/java/web_pages/TutByHomePage.java
+++ src/test/java/web_pages/TutByHomePage.java
@@ -1,9 +1,14 @@
 package web_pages;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.Utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 public class TutByHomePage {
@@ -17,6 +22,11 @@ public class TutByHomePage {
 
     public void loginToSite(){
         driver.get("https://www.tut.by");
+        try {
+            takeScreenshot("src/test/resources/screen.png");
+        } catch (IOException ex) {
+            System.out.println("Error to file creating while screening");
+        }
         driver.findElement(By.cssSelector("div#authorize a")).click();
         driver.findElement(By.name("login")).sendKeys(properties.getProperty("USER_NAME"));
         driver.findElement(By.name("password")).sendKeys(properties.getProperty("PASSWORD"));
@@ -34,5 +44,10 @@ public class TutByHomePage {
 
     public Boolean authLinkIsDisplayed(){
         return driver.findElement(By.cssSelector("div#authorize a")).isDisplayed();
+    }
+
+    private void takeScreenshot(String pathname) throws IOException {
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File(pathname));
     }
 }
